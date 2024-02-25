baf4659945d16f6d8cbec4525ef55e8fbf66c860
Ascoos Development <ascoos@users.noreply.github.com>
2024-02-25 18:31:50 +0200

    Updated to 1.0.1

---
+++ languages/el-gr.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ *   __ _  ___  ___ ___   ___   ___     ____ _ __ ___   ___
+ *  / _` |/  / / __/ _ \ / _ \ /  /    / __/| '_ ` _ \ /  /
+ * | (_| |\  \| (_| (_) | (_) |\  \   | (__ | | | | | |\  \
+ *  \__,_|/__/ \___\___/ \___/ /__/    \___\|_| |_| |_|/__/
+ * 
+ * 
+ ************************************************************************************
+ * @ASCOOS-NAME        	: ASCOOS CMS 24'                                            *
+ * @ASCOOS-VERSION     	: 24.0.0                                                    *
+ * @ASCOOS-CATEGORY    	: Block (Frontend and Administrator Side)                   *
+ * @ASCOOS-CREATOR     	: Drogidis Christos                                         *
+ * @ASCOOS-SITE        	: www.ascoos.com                                            *
+ * @ASCOOS-LICENSE     	: [Commercial] http://docs.ascoos.com/lics/ascoos/AGL.html  *
+ * @ASCOOS-COPYRIGHT   	: Copyright (c) 2007 - 2024, AlexSoft Software.             *
+ ************************************************************************************
+ *
+ * @package            	: Up Previous Contents 
+ * @subpackage         	: Languages - Greek Languages (el, el-GR)
+ * @source             	: /[BLOCKS PATH]/up_prev_contents/languages/el-GR.php
+ * @fileNo				: 7
+ * @version            	: 1.0.1
+ * @created            	: 2012-08-06 20:00:00 UTC+3
+ * @updated            	: 2024-02-25 12:00:00 UTC+3
+ * @author             	: Drogidis Christos
+ * @authorSite         	: www.alexsoft.gr
+ * @license 			: Freeware
+ * @since 				: 24.0.0
+ * @translator	       	:
+ * @translatorEmail    	:
+ * @translatorSite     	:
+ * @translatorCreated  	:
+ * @translatedUpdated  	:
+ */
+
+defined ("ALEXSOFT_RUN_CMS") or die("Prohibition of Access.");
+
+class TBlock_up_prev_content_Language extends \TObject
+{
+	public $blockname = "Πρόταση ανάγνωσης προηγούμενου άρθρου";
+	public $close = "Κλείσιμο";
+	public $promote = "Το block αυτό, είναι επίσημη επέκταση του <a href=\"http://www.ascoos.com\">Ascoos Cms</a>.";
+	
+
+	public $APL_license = "Ascoos General License (AGL)";
+	public $APL_author = "ΔΡΟΓΚΙΔΗΣ ΧΡΗΣΤΟΣ";
+	public $APL_creation = "";
+	public $APL_copyright = "ALEXSOFT ΛΟΓΙΣΜΙΚΟ";
+	public $APL_translator = "";
+	public $APL_translatorEmail = "";
+	public $APL_translatorUrl = "";
+	public $APL_desc = "Η ενότητα (Block) αυτή, εμφανίζει ένα πλαίσιο που περιέχει ένα ή περισσότερα προηγούμε άρθρα σε σχέση με το τρέχων.";
+
+	public $APL_paramgroup_general_label = "▼ &nbsp; Γενικοί Παράμετροι";
+	public $APL_paramgroup_general_label_info = "<strong>ΓΕΝΙΚΟΙ ΠΑΡΑΜΕΤΡΟΙ</strong><br />--------------------------------------<br /><br />Στο πλαίσιο αυτό μπορείτε να διαμορφώσετε τις γενικές παραμέτρους εμφάνισης της ενότητας.";
+	public $APL_count_label = "Αριθμός άρθρων";
+	public $APL_count_desc = "<strong>ΑΡΙΘΜΟΣ ΑΡΘΡΩΝ</strong><br />--------------------------------------<br /><br />Εδώ πρέπει να επιλέξετε τον αριθμό των άρθρων που θα εμφανιστούν";
+	public $APL_str_length_label = "Μέγιστο Μήκος Κειμένου";
+	public $APL_str_length_desc = "<strong>ΜΗΚΟΣ ΚΕΙΜΕΝΟΥ</strong><br />--------------------------------------<br /><br />Εδώ πρέπει να ορίσετε το μέγιστο μήκος που θα έχει το εισαγωγικό κείμενο κάθε άρθρου.";
+	public $APL_promote_label = "Προβολή";
+	public $APL_promote_desc = "<strong>ΠΡΟΒΟΛΗ</strong><br />--------------------------------------<br /><br />Εδώ μπορείτε να επιλέξετε εάν στο πλαίσιο θα συμπεριληφθεί κείμενο προβολής του κατασκευαστή";
+
+	public $APL_theme_label = "Θέμα εμφάνισης Ενότητας";
+	public $APL_theme_placeholder = "Επιλέξτε θέμα εμφάνισης της ενότητας";
+	public $APL_theme_desc = "<strong>ΘΕΜΑ ΕΜΦΑΝΙΣΗΣ ΕΝΟΤΗΤΑΣ</strong><br />--------------------------------------<br /><br />Επιλέξτε το θέμα εμφάνισης που θα υλοποιηθεί η ενότητα &laquo;%s&raquo;.<br /><br />Κάθε ενότητα έχει τουλάχιστο το προκαθορισμένο θέμα εμφάνισης με την ονομασία &laquo;Default&raquo;";
+
+	public $APL_paramgroup_categories_label = "▼ &nbsp; Επιλογές Κατηγοριών";
+	public $APL_paramgroup_categories_label_info = "<strong>ΕΠΙΛΟΓΕΣ ΚΑΤΗΓΟΡΙΩΝ</strong><br />--------------------------------------<br /><br />Στο πλαίσιο αυτό μπορείτε να διαμορφώσετε τις παραμέτρους προέλευσης των περιεχομένων της ενότητας";
+	public $APL_type_label = "Τύποι άρθρων";
+	public $APL_type_placeholder = "Επιλέξτε τύπους άρθρων";
+	public $APL_type_desc = "<strong>ΤΥΠΟΙ ΑΡΘΡΩΝ</strong><br />--------------------------------------<br /><br />Ποιοι τύποι άρθρων θέλετε να συμπεριληφθούν; Αυτό θα έχει ως αποτέλεσμα την εμφάνιση μόνο των άρθρων που έχουν ώς τύπο έναν από τους οριζόμενους.";
+	public $APL_cat_ids_label = "Κατηγορίες που θα περιληφθούν";
+	public $APL_cat_ids_placeholder = "Επιλέξτε Κατηγορίες Άρθρων";
+	public $APL_cat_ids_desc = "<strong>ΚΑΤΗΓΟΡΙΕΣ ΠΟΥ ΘΑ ΠΕΡΙΛΗΦΘΟΥΝ</strong><br />--------------------------------------<br /><br />Ποιες κατηγορίες θέλετε να περιληφθούν;<br /><br />Αυτό θα έχει ως αποτέλεσμα μόνο τα άρθρα που έχουν μια από τις κατηγορίες αυτές να εμφανιστούν στην λίστα των πρόσφατων άρθρων.";
+	public $APL_except_cat_ids_label = "Κατηγορίες που θα εξαιρεθούν";
+	public $APL_except_cat_ids_placeholder = "Επιλέξτε Κατηγορίες Άρθρων";
+	public $APL_except_cat_ids_desc = "<strong>ΚΑΤΗΓΟΡΙΕΣ ΠΟΥ ΘΑ ΕΞΑΙΡΕΘΟΥΝ</strong><br />--------------------------------------<br /><br />Ποιες κατηγορίες θέλετε να εξαιρεθούν;<br /><br />Αυτό θα έχει ως αποτέλεσμα να παρακαμφθούν όλα τα άρθρα που ανήκουν στις κατηγορίες αυτές.<br /><br />Η επιλογή αυτή αναιρεί την κατηγορία από την παραπάνω παράμετρο (περιλαμβανόμενες κατηγορίες), εάν δοθεί και σε αυτή.";
+
+
+	public $APL_paramgroup_animation_label = "▼ &nbsp; Παράμετροι Λεπτομερειών Εμφάνισης Πλαισίου";
+	public $APL_paramgroup_animation_label_info = "<strong>ΠΑΡΑΜΕΤΡΟΙ ΛΕΠΤΟΜΕΡΕΙΩΝ</strong><br />--------------------------------------<br /><br />Στο πλαίσιο αυτό μπορείτε να διαμορφώσετε τις παραμέτρους εμφάνισης του πλαισίου.";
+	public $APL_animation_label = "Εφέ Εμφάνισης";
+	public $APL_animation_placeholder = " Επιλέξτε ένα εφέ εμφάνισης ";
+	public $APL_animation_desc = "<strong>ΕΦΕ ΕΜΦΑΝΙΣΗΣ</strong><br />--------------------------------------<br /><br />Εδώ πρέπει να επιλέξετε ένα από τα διαθέσιμα εφέ εμφάνισης του πλαισίου.";
+	public $APL_opt_fade = "Αργή Εμφάνιση / Αργό Σβήσιμο (Fade)";
+	public $APL_opt_flyout = "Πέταγμα προς τα έξω (FlyOut)";
+	
+	public $APL_corner_label = "Γωνία Εμφάνισης";
+	public $APL_corner_desc = "<strong>ΓΩΝΙΑ ΕΜΦΑΝΙΣΗΣ</strong><br />--------------------------------------<br /><br />Επιλέξτε την θέση εμφάνισης του πλαισίου.<br /><br />Στην παρούσα έκδοση υποστηρίζονται μόνο 2 επιλογές θέσης.";
+	public $APL_corner_placeholder = " Επιλέξτε μια γωνία εμφάνισης ";
+	public $APL_opt_LeftBottom = "Κάτω Αριστερά";
+	public $APL_opt_RightBottom = "Κάτω Δεξιά";
+	public $APL_element_label = "Στοιχεία Μετατόπισης Έναρξης";
+	public $APL_element_desc = "<strong>ΣΤΟΙΧΕΙΑ ΜΕΤΑΤΟΠΙΣΗΣ ΕΝΑΡΞΗΣ</strong><br />--------------------------------------<br /><br />Εδώ μπορείτε να επιλέξετε τα στοιχεία από τα οποία θα ξεκινήσει η εμφάνιση του πλαισίου.<br /><br />Εάν επιλέξετε πάνω από ένα στοιχείο τότε θα πρέπει να χωρίζονται με κόμμα.<br /><br />Για επιλογή κλάσης θα πρέπει να υπάρχει μπροστά η τελεία [.] και για ID το [#].";
+	public $APL_percent_label = "Ποσοστό μετατοπισμένης θέσης έναρξης (%)";
+	public $APL_percent_desc = "<strong>ΠΟΣΟΣΤΟ ΜΕΤΑΤΟΠΙΣΗΣ</strong><br />--------------------------------------<br /><br />Εάν δεν έχετε επιλέξει στην προηγούμενη παράμετρο κάποιο στοιχείο, τότε μπορείτε εδώ να ορίσετε το ποσοστό του συνολικού ύψους της σελίδας βάσει του οποίου θα αρχίσει να εμφανίζεται το πλαίσιο.";
+	public $APL_width_label = "Μήκος Πλαισίου (pixels)";
+	public $APL_width_desc = "<strong>ΜΗΚΟΣ ΠΛΑΙΣΙΟΥ</strong><br />--------------------------------------<br /><br />Εδώ θα πρέπει να δώσετε το μήκος του πλαισίου σε pixels. Θα πρέπει να είναι μόνο ο αριθμός χωρίς το [px]";
+	public $APL_side_label = "Απόσταση πλαισίου από τις γωνίες (pixels)";
+	public $APL_side_desc = "<strong>ΑΠΟΣΤΑΣΗ ΑΠΟ ΓΩΝΙΕΣ</strong><br />--------------------------------------<br /><br />Εδώ θα πρέπει να δώσετε σε pixels (μόνο αριθμητικά) την οριζόντια και κάθετη απόσταση από τις γωνίες του παραθύρου.";
+	public $APL_target_label = "Άνοιγμα συνδέσμου σε νέο παράθυρο;";
+	public $APL_target_desc = "<strong>ΑΝΟΙΓΜΑ ΣΕ ΝΕΟ ΠΑΡΑΘΥΡΟ</strong><br />--------------------------------------<br /><br />Εδώ μπορείτε εάν θέλετε να ορίσετε να ανοίγει το άρθρο σε νέο παράθυρο";
+	public $APL_show_btn_close_label = "Εμφάνιση εικονιδίου κλεισίματος;";
+	public $APL_show_btn_close_desc = "<strong>ΕΜΦΑΝΙΣΗ ΕΙΚΟΝΙΔΙΟΥ</strong><br />--------------------------------------<br /><br />Εδώ μπορείτε να επιλέξετε την εμφάνιση εικονιδίου κλεισίματος στο πλαίσιο. Σε κάθε περίπτωση το πλαίσιο κλείνει αυτόματα όταν δεν συντρέχουν οι όροι που έχετε ορίσει στις παρούσες παραμέτρους, π.χ. όταν μεταφέρεστε στην κορυφή της σελίδας.";
+}
+?>
---
+++ languages/en.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ *   __ _  ___  ___ ___   ___   ___     ____ _ __ ___   ___
+ *  / _` |/  / / __/ _ \ / _ \ /  /    / __/| '_ ` _ \ /  /
+ * | (_| |\  \| (_| (_) | (_) |\  \   | (__ | | | | | |\  \
+ *  \__,_|/__/ \___\___/ \___/ /__/    \___\|_| |_| |_|/__/
+ * 
+ * 
+ ************************************************************************************
+ * @ASCOOS-NAME        	: ASCOOS CMS 24'                                            *
+ * @ASCOOS-VERSION     	: 24.0.0                                                    *
+ * @ASCOOS-CATEGORY    	: Block (Frontend and Administrator Side)                   *
+ * @ASCOOS-CREATOR     	: Drogidis Christos                                         *
+ * @ASCOOS-SITE        	: www.ascoos.com                                            *
+ * @ASCOOS-LICENSE     	: [Commercial] http://docs.ascoos.com/lics/ascoos/AGL.html  *
+ * @ASCOOS-COPYRIGHT   	: Copyright (c) 2007 - 2024, AlexSoft Software.             *
+ ************************************************************************************
+ *
+ * @package            	: Up Previous Contents 
+ * @subpackage         	: Languages - Greek Languages (el, el-GR)
+ * @source             	: /[BLOCKS PATH]/up_prev_contents/languages/en.php
+ * @fileNo				: 8
+ * @version            	: 1.0.1
+ * @created            	: 2012-08-06 20:00:00 UTC+3
+ * @updated            	: 2024-02-25 12:00:00 UTC+3
+ * @author             	: Drogidis Christos
+ * @authorSite         	: www.alexsoft.gr
+ * @license 			: Freeware
+ * @since 				: 24.0.0
+ * @translator	       	: Kourtidis George
+ * @translatorEmail    	:
+ * @translatorSite     	: https://www.youtube.com/@DolceVii
+ * @translatorCreated  	: 2012-08-06 20:00:00 UTC+2
+ * @translatedUpdated  	:
+ */
+
+defined ("ALEXSOFT_RUN_CMS") or die("Prohibition of Access.");
+
+class TBlock_up_prev_content_Language extends \TObject
+{
+	public $blockname = "Up Previous Contents";
+	public $close = "Close";
+	public $promote = "The block it is official extension of <a href=\"http://www.ascoos.com\">Ascoos Cms</a>.";
+	
+
+	public $APL_license = "Ascoos General License (AGL)";
+	public $APL_author = "DROGIDIS CHRISTOS";
+	public $APL_creation = "";
+	public $APL_copyright = "ALEXSOFT SOFTWARE";
+	public $APL_translator = "";
+	public $APL_translatorEmail = "";
+	public $APL_translatorUrl = "";
+	public $APL_desc = "The section (Block) that displays a frame containing one or more previous articles relative to the current.";
+
+	public $APL_paramgroup_general_label = "▼ &nbsp; General Parameters";
+	public $APL_paramgroup_general_label_info = "<strong>GENERAL PARAMETERS</strong><br />--------------------------------------<br /><br />In this context, you can configure the general parameters of the display section";
+	public $APL_count_label = "Number of articles";
+	public $APL_count_desc = "<strong>NUMBER OF ARTICLES</strong><br />--------------------------------------<br /><br />Here you must select the number of articles that will appear";
+	public $APL_str_length_label = "Maximum Text Length";
+	public $APL_str_length_desc = "<strong>TEXT LENGTH</strong><br />--------------------------------------<br /><br />Here you must set the maximum length will be the introductory text of each article.";
+	public $APL_promote_label = "View";
+	public $APL_promote_desc = "<strong>VIEW</strong><br />--------------------------------------<br /><br />Here you can choose whether to be included in the text display of the manufacturer";
+
+	public $APL_theme_label = "Section Theme";
+	public $APL_theme_placeholder = "Choose a theme show of section";
+	public $APL_theme_desc = "<strong>SECTION THEME</strong><br />--------------------------------------<br /><br />Select the subject of appearance that will be materialised the section &laquo;%s&raquo;.<br /><br />Each module has at least fixed display issue with the name &laquo;Default&raquo;";
+
+	public $APL_paramgroup_categories_label = "▼ &nbsp; Choices of Categories";
+	public $APL_paramgroup_categories_label_info = "<strong>CHOICES OF CATEGORIES</strong><br />--------------------------------------<br /><br />In this context, you can configure the contents of the source section";
+	public $APL_type_label = "Types of Articles";
+	public $APL_type_placeholder = "Select types of articles";
+	public $APL_type_desc = "<strong>TYPES OF ARTICLES</strong><br />--------------------------------------<br /><br />What types of articles you want to include? This will result in only the articles that have as type one of them nominated.s";
+	public $APL_cat_ids_label = "Categories will be included";
+	public $APL_cat_ids_placeholder = "Select Category Articles";
+	public $APL_cat_ids_desc = "<strong>CATEGORIES TO BE INCLUDED</strong><br />--------------------------------------<br /><br />What categories you want to include?<br /><br />This will only have as result the articles that have one from this categories of presenting itself in the list of recent articles.";
+	public $APL_except_cat_ids_label = "Categories to be excluded";
+	public $APL_except_cat_ids_placeholder = "Select Category Articles";
+	public $APL_except_cat_ids_desc = "<strong>CATEGORIES TO BE EXCLUDED</strong><br />--------------------------------------<br /><br />Which category you want to exclude?<br /><br />This will lead to sidestep all the articles in these categories.<br /><br />This option negates the category of the above parameter (categories included), if paid in it.";
+
+
+	public $APL_paramgroup_animation_label = "▼ &nbsp; Parameters details Display Frame";
+	public $APL_paramgroup_animation_label_info = "<strong>PARAMETERS DETAILS</strong><br />--------------------------------------<br /><br />In this context, you can configure the appearance of the frame.";
+	public $APL_animation_label = "effects Template";
+	public $APL_animation_placeholder = " Select an effect show ";
+	public $APL_animation_desc = "<strong>EFFECT DISPLAY</strong><br />--------------------------------------<br /><br />Here you must select one of the available display effects of the frame.";
+	public $APL_opt_fade = "Slow Show / Off Slow (Fade)";
+	public $APL_opt_flyout = "Throw out (FlyOut)";
+	
+	public $APL_corner_label = "Viewing angle";
+	public $APL_corner_desc = "<strong>VIEWING ANGLE</strong><br />--------------------------------------<br /><br />Select the display position of the frame.<br /><br />In this version only supports 2 position options.";
+	public $APL_corner_placeholder = " Choose a display angle ";
+	public $APL_opt_LeftBottom = "Bottom Left";
+	public $APL_opt_RightBottom = "Bottom Right";
+	public $APL_element_label = "Opening Displacement Data";
+	public $APL_element_desc = "<strong>POSITION OPENING LEMENTS</strong><br />--------------------------------------<br /><br />Here you can select the elements of which will start the appearance of the frame.<br /><br />If you select more than one item then must be separated by commas.<br /><br />To select class should be the front of the dot [.] And the ID [#].";
+	public $APL_percent_label = "Percentage of displaced position (%)";
+	public $APL_percent_desc = "<strong>RATE POSITION</strong><br />--------------------------------------<br /><br />if you have not then selected in the previous parameter any element, you can here fix the percentage of total height of page whereby they begin to show the context.";
+	public $APL_width_label = "Frame Length (pixels)";
+	public $APL_width_desc = "<strong>LENGTH FRAME</strong><br />--------------------------------------<br /><br />Here you should give the length of the frame in pixels. It should be just the number without [px]";
+	public $APL_side_label = "Distance frame corners (pixels)";
+	public $APL_side_desc = "<strong>DISTANCE FROM CORNERS</strong><br />--------------------------------------<br /><br />Here you should give in pixels (only numeric) horizontal and vertical distance from the corners of the window.";
+	public $APL_target_label = "Open link in new window?";
+	public $APL_target_desc = "<strong>OPENS IN NEW WINDOW</strong><br />--------------------------------------<br /><br />Here you can specify whether you want to open the article in a new window";
+	public $APL_show_btn_close_label = "Show icon closing?";
+	public $APL_show_btn_close_desc = "<strong>SHOW ICON</strong><br />--------------------------------------<br /><br />Here you can choose to display icon in closing. In each case the frame closes automatically when not fulfilled the conditions you set in these parameters, eg when taken to the top of the page.";
+}
+?>
